31e592ed7055662f1dae1ac99cb6b3940a2baed2
kanizsairita <kanizsairita@gmail.com>
2019-11-25 13:07:53 +0100

    adds Person class

---
+++ src/com/company/Person.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Person {
+
+    private String name;
+    private String city;
+    private String email;
+
+    public Person(String name, String city, String email) {
+        this.name = name;
+        this.city = city;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
