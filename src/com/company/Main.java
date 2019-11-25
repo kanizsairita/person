57d7d3251c827fc8982af84fe26d67dc0bdd8ecd
Rita <kanizsairita@gmail.com>
2019-11-25 14:24:43 +0100

    new method printEmails()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,22 +7,28 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
+    private  static List<Person> sortedPeople;
+
+
     public static void main(String[] args) throws FileNotFoundException {
 
-        List<Person> sortedPeople=Reader.read("src/us-500.csv");
+        sortedPeople=Reader.read("src/us-500.csv");
         Set<Person> sortedPeopleSet=new TreeSet<>();
         sortedPeopleSet.addAll(sortedPeople);
 
 
         System.out.println("A Chicago-ban élő emberek email-címei:");
-        List<Person> chicagoList= Filter.cityFilter("Chicago", sortedPeople);
-        for (Person person : chicagoList) {
-            System.out.println(person.getEmail());
-        }
-
+        printEmails("Chicago");
 
     }
 
+    public static void printEmails(String city){
+
+        List<Person> cityList= Filter.cityFilter(city, sortedPeople);
+        for (Person person : cityList) {
+            System.out.println(person.getEmail());
+        }
+    }
 
 
 }
